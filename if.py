e900c3943df3945f9d599fb73ea872902ed968f5
Aromalsukumar <aromalarom2266@gmail.com>
2024-11-04 15:44:44 +0530

    python update

---
+++ if.py
@@ -0,0 +1,7 @@
+age=int(input("enter age"))
+if 18<age:
+    print("vote")
+elif age==18:
+    print("first vote")   
+else:
+    print("not vote")    
---
+++ sumof.py
@@ -0,0 +1,15 @@
+a=int(input("enter a number"))
+b=a%10
+if b==3:
+    s=a+3
+    print(s)
+elif b==5:
+    s=a+5
+    print(s)    
+elif b==8:
+    s=a+8
+    print(s)
+else:
+    s=a+10
+    print(s)    
+
