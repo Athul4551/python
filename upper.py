5d8c3b596cafe6e56c12c2bc6465319ca6d8d36e
Aromalsukumar <aromalarom2266@gmail.com>
2024-11-06 15:55:24 +0530

    python update

--- upper.py
+++ upper.py
@@ -23,13 +23,30 @@ print(b)
 # txt =input("enter a sentence")
 # b=input("enter  word")
 # c=input("enter replace word")
-# # c =txt.endswith(b)
-
-# x = txt.replace(b, c)
-
+# c =txt.endswith(b)
+# x = txt.replace(b,c)
 # print(x)
+txt =input("enter a sentence")
+x = txt.split()
+print(x)
+y ="".join(x)
+print(y)
+for i in y:
+  print(i)
 # txt =input("enter a sentence")
 # x = txt.split()
 # print(x)
-# y = " ".join(x)
-# print(y)
+# list1=["anit","edwin","jiya","sandra"]
+# c=" ".join(list1)
+# print(c)
+# a=input("enter a sentence")
+# b=input("enter  word")
+# c=a.split(" ")
+# for i in c:
+#      if i==b:
+#         print("word exist")
+#         break
+# else:
+#     print("does not exist")    
+     
+
