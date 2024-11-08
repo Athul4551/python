09565f7a0ecd5db329da39f61284daf26f0b2fc7
Aromalsukumar <aromalarom2266@gmail.com>
2024-11-08 15:39:11 +0530

    python

--- patte.py
+++ patte.py
@@ -9,13 +9,13 @@
 #         a=a+1
 #         print(a,end=" ")
 #     print("")
-a=6
-for i in range(6):
-    a=a-1
-    print(end=" "*a)
-    for j in range(i+1):
-        print(j+1,end=" ")
-    print("")  
+# a=6
+# for i in range(6):
+#     a=a-1
+#     print(end=" "*a)
+#     for j in range(i+1):
+#         print(j+1,end=" ")
+#     print("")  
 # a=6
 # for i in range(6):
 #     a=a-1
@@ -23,8 +23,33 @@ for i in range(6):
 #     for j in range(i+1):
 #          print(j,end=" ")
 #     print(" ")
-       
-       
+# a=6
+# for i in range(1,6):
+#     a=a-1
+#     print(end=" "*a)
+#     for j in range(1,6):
+#          print("*"*i,end=" ")
+#     print("")         
+a=6
+c=1
+d=9
+e=1       
+for i in range(1,6):
+    a=a-1
+    print(end=" "*a)
+    print("*"*c)
+    c=c+2
+    
+for k in range(1,6):
+    print(end=" "*e)
+    e=e+1
+    print("*"*d)
+    d=d-2
+# for i in range(6,1,-2):  
+#     print("*"*d)
+#     print(end=" "*a)
+#     e=e+1
+#     d=d-2       
     
     
     
---
+++ while.py
@@ -0,0 +1,14 @@
+n=int(input("enter a number"))
+e=n
+s=0
+d=0
+while n>0:
+    s=n%10
+    d=d*10+s
+    n=n//10
+# print(d)    
+if(e==d):
+    print("is palindrom")
+else:
+    print("not palindrom")       
+
