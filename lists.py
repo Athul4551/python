d0b712ff6b016a924c01de243c7b6727c2f94506
Aromalsukumar <aromalarom2266@gmail.com>
2024-11-15 15:27:07 +0530

    python

--- lists.py
+++ lists.py
@@ -7,9 +7,23 @@
 # b=len(a)
 # for i in range(b):
 #     print(a[i])
-a=["apple",1,"orang",[2,3,5,6],"f",5,6]
-print(a[:3])
-a[1]="grape"
-print(a[0:])
-a[1:3]=4,2,"mango"
-print(a[0:])
+# a=["apple",1,"orang",[2,3,5,6],"f",5,6]
+# print(a[:3])
+# a[1]="grape"
+# print(a[0:])
+# a[1:3]=4,2,"mango"
+# print(a[0:])
+a=["apple",1,"orang",[2,3,5,6]]
+a.append("melon")
+print(a)
+a.insert(1,"mango")
+print(a)
+b=["apple",1,"orang",[2,3,5,6],"f",5,6]
+a.extend(b)
+print(a)
+a.pop(0)
+print(a)
+a.clear()
+print(a)
+del a
+print(a)
