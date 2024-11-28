124430aa8444d61a011791ac094f5d95b2de9f50
Aromalsukumar <aromalarom2266@gmail.com>
2024-11-28 15:22:51 +0530

    python

---
+++ mulinher.py
@@ -0,0 +1,70 @@
+# multivevel
+# class Ve:
+#     def work():
+#         print("move")
+# class Car(Ve):
+#     def wheel():
+#         print("four wheel")
+# class Volvo(Car):
+#     def myl():
+#         print(19)                
+# obj1=Volvo
+# obj1.work()
+# obj1.myl()
+# multiple
+# class Tove:
+#     def work():
+#         print("move")
+# class Fourve():
+#     def wheel():
+#         print("four wheel")
+# class Threeve(Tove,Fourve):
+#     def myl():
+#         print(19)                
+# obj1=Threeve
+# obj1.work()
+# obj1.myl()
+# obj1.wheel()
+# hierartical
+# class Tove:
+#     def work():
+#         print("move")
+# class Fourve(Tove):
+#     def wheel():
+#         print("four wheel")
+# class Threeve(Tove):
+#     def myl():
+#         print(19)                
+# obj1=Fourve
+# obj2=Threeve
+# obj1.work()
+# obj2.work()
+# obj2.myl()
+# obj1.wheel()
+class A1:
+    def a11():
+        print("A1")
+class B1(A1):
+    def b11():
+        print("B1")
+class C1(B1):
+    def c11():
+        print("C1")
+class D1(B1):
+    def d11():
+        print("D1")
+class E1(D1):
+    def e11():
+        print("E1")
+class F1(C1,D1):
+    def f11():
+        print("F1")
+obj=F1
+obj1=E1
+obj.a11()
+obj.b11()
+obj.c11()
+obj.d11()
+obj1.e11()                                        
+obj.f11()
+                
---
+++ sinhertance.py
@@ -0,0 +1,10 @@
+class Ve:
+    def work():
+        print("move")
+class Car(Ve):
+    def wheel():
+        print("four wheel")
+obj1=Car()
+obj1.work()        
+                
+        
