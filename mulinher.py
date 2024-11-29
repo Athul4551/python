6a02c191105354c05f5190aa2bf3dad4c1c90c09
Aromalsukumar <aromalarom2266@gmail.com>
2024-11-29 15:48:23 +0530

    python

--- mulinher.py
+++ mulinher.py
@@ -41,30 +41,31 @@
 # obj2.work()
 # obj2.myl()
 # obj1.wheel()
-class A1:
-    def a11():
-        print("A1")
-class B1(A1):
-    def b11():
-        print("B1")
-class C1(B1):
-    def c11():
-        print("C1")
-class D1(B1):
-    def d11():
-        print("D1")
-class E1(D1):
-    def e11():
-        print("E1")
-class F1(C1,D1):
-    def f11():
-        print("F1")
-obj=F1
-obj1=E1
-obj.a11()
-obj.b11()
-obj.c11()
-obj.d11()
-obj1.e11()                                        
-obj.f11()
+# class A1:
+#     def a11():
+#         print("A1")
+# class B1(A1):
+#     def b11():
+#         print("B1")
+# class C1(B1):
+#     def c11():
+#         print("C1")
+# class D1(B1):
+#     def d11():
+#         print("D1")
+# class E1(D1):
+#     def e11():
+#         print("E1")
+# class F1(C1,D1):
+#     def f11():
+#         print("F1")
+# obj=F1
+# obj1=E1
+# obj.a11()
+# obj.b11()
+# obj.c11()
+# obj.d11()
+# obj1.e11()                                        
+# obj.f11()
+
                 
---
+++ poly.py
@@ -0,0 +1,10 @@
+class Jiya():
+    def ji1():
+        print("hello jiya")
+class Sandra(Jiya):
+    def ji1():
+        print("hello sandra")
+obj1=Jiya
+obj2=Sandra
+obj1.ji1()
+obj2.ji1()                
